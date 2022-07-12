b94aa24bcfa9bd2f234246438688318dd817c89c
eusha <yafaseusha02@gmail.com>
2022-07-12 12:58:22 +0600

    slight change on interface

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import pyshorteners
-long_url = input("Enter the URL to shorten: ")
+long_url = input("\nEnter the URL to shorten:\n")
  
 shortener = pyshorteners.Shortener()
 
 short_url = shortener.chilpit.short(long_url)
 
 
-print("The Shortened URL is: " + short_url)
+print("\nThe Shortened URL is:\n " + short_url)
